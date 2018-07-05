6d036107c20f2132aa5a08119a373f81d4fb175d
pc386 <p.colley@kent.ac.uk>
2018-07-05 09:52:55 +0100

    run test

--- test/actionwords.js
+++ test/actionwords.js
@@ -13,7 +13,7 @@ exports.Actionwords = {
     driver = await new webdriver.Builder().withCapabilities(chromeCapabilities).build();
     await driver.get('https://www.google.com/')
     
-      assert.equal(await driver.getTitle(), "Google")
+      assert.equal(await driver.getTitle(), "Googrle")
      
   },
   goToTheLookupsPage: function () {
